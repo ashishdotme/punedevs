3a78ca3cc04b857f1de4572e08de2cb316d81c27
Ashish Patel <ashishsushilpatel@gmail.com>
2018-12-02 14:20:40 +0530

    Add post method for registration

--- server.js
+++ server.js
@@ -1,15 +1,35 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 
 // DB config
 const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
 mongoose
-  .connect(db)
+  .connect(db, { 
+    "auth": {"authSource": "admin"},
+    useNewUrlParser: true })
   .then(() => console.log("Mongodb connected"))
   .catch((error) => console.log(error));
+
+
+// Use routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
 
 app.get('/', (req, res) => res.send('Hello world'));
 
