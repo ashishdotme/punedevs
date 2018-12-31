ab7dfa4228473f606edd5102272d701151ac5e39
Ashish Patel <ashishsushilpatel@gmail.com>
2018-12-31 13:36:04 +0530

    Polish design of profile list

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -7,37 +7,49 @@ class ProfileItem extends Component {
     const { profile } = this.props;
     return (
       <div className="card card-body bg-light mb-3">
-        <div className="row">
-          <div className="col-md-2 col-sm-8">
-            <img src={profile.user.avatar} alt="" className="rounded-circle" />
-          </div>
-          <div className="col-lg-6 col-md-8 ml-5">
-            <h3>{profile.user.name}</h3>
-            <p>
+        <div className="grid">
+          <div className="row">
+            <div className="col-lg-3 col-md-8 col-xs-12 col-12">
+              <img
+                src={profile.user.avatar}
+                alt=""
+                className="rounded-circle"
+              />
+            </div>
+            <div className="col-lg-4 col-md-4 col-xs-12 col-12 ml-3">
+              <h3 className="pt-3">{profile.user.name}</h3>
               {profile.status}
               {isEmpty(profile.company) ? null : (
                 <span> at {profile.company}</span>
               )}
-            </p>
-            <p>
+              <br />
               {isEmpty(profile.location) ? null : (
                 <span>{profile.location}</span>
               )}
-            </p>
-            <Link to={`profile/${profile.handle}`} className="btn btn-info">
-              View Profile
-            </Link>
-          </div>
-          <div className="col-md-2">
-            <h4>Skill Set</h4>
-            <ul className="list-group">
-              {profile.skills.slice(0, 4).map((skill, index) => (
-                <li key={index} className="card-header">
-                  <i className="fa fa-check pr-1" />
-                  {skill}
-                </li>
-              ))}
-            </ul>
+              <br />
+              <br />
+              <Link
+                to={`profile/${profile.handle}`}
+                className="btn btn-primary"
+              >
+                View Profile
+              </Link>
+            </div>
+            <div className="col-md-4 d-none d-md-block pt-3">
+              <h4>Skill Set</h4>
+              <ul className="list-group">
+                {profile.skills.slice(0, 4).map((skill, index) => (
+                  <li
+                    key={index}
+                    className="card-header"
+                    style={{ "list-style": "none" }}
+                  >
+                    <i className="fa fa-laptop pr-2" />
+                    {skill}
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
       </div>
